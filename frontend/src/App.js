5268f92a957d399719967129661044b93d09b0d7
Sandrine Elander <s.elander@mindstorming.se>
2025-02-21 18:22:40 +0100

    Corrected typo in BASE_URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import UserDetails from "./components/UserDetails";
 
 // const BASE_URL = "http://localhost:8080/";
 // const BASE_URL = "https://max-sandrine-auth-api.herokuapp.com/";
-const BASE_URL = "https://project-auth-518979433183.europe-north1.run.app";
+const BASE_URL = "https://project-auth-518979433183.europe-north1.run.app/";
 const SIGNUP_URL = `${BASE_URL}users`;
 const LOGIN_URL = `${BASE_URL}sessions`;
 
